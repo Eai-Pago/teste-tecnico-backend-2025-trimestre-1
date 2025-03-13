20d15fecf1971ae55af311b2868f73b84b9f4677
Gabriel Zaninotti <gabriel@zaninotti.net>
2025-03-13 20:53:17 -0300

    Alterações para primeiro build
    + gitignore
    > corrigido "__dirname not defined"
    > corrigido erro na hora de buildar via docker-compose

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const multer = require('multer');
-const redis = require('redis');
+import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import videoRouter from './controllers/videoController.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/uploads', express.static(join(__dirname, 'uploads')));
+
+app.use('/api', videoRouter);
+
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-  });
-  
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
